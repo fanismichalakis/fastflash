490976105b6c800ac489c5cba9e33d270541733e
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-02 14:46:56 +0100

    remove helloWolrd add fake data

--- fastflash/src/App.vue
+++ fastflash/src/App.vue
@@ -10,7 +10,50 @@ export default {
   name: 'App',
   components: {
     Card
-  }
+  },
+  data() {
+    return {
+      cards_id: [1000, 1001, 1002, 1003],
+      cards_info: {
+        1000: {
+          recto:'manger',
+          verso:'eat',
+          recto_def:'français',
+          verso_def:'anglais',
+          position:1,
+          library:'franglais',
+          id:1000
+        },
+        1001: {
+          recto:'bouger',
+          verso:'move',
+          recto_def:'français',
+          verso_def:'anglais',
+          position:1,
+          library:'franglais',
+          id:1001
+        },
+        1002: {
+          recto:'chien',
+          verso:'dog',
+          recto_def:'français',
+          verso_def:'anglais',
+          position:1,
+          library:'franglais',
+          id:1002
+        },
+        1003: {
+          recto:'jaune',
+          verso:'yellow',
+          recto_def:'français',
+          verso_def:'anglais',
+          position:1,
+          library:'franglais',
+          id:1003
+        }
+      },
+    };
+  },
 }
 </script>
 
