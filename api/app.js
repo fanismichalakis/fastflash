36b1c1bb62e768fce8a8d9dbdc9adde8ff719022
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-02 13:44:59 +0100

    import variable MONGODB from .env file

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose= require('mongoose');
 
-mongoose.connect('mongodb+srv://fastflash_admin:fastflash@fastflash.d1nrx.mongodb.net/fastflash?retryWrites=true&w=majority',  {useNewUrlParser: true}, { useUnifiedTopology: true } )
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 });
