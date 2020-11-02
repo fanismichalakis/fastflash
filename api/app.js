50ce1769a5f0caf834dd7479abb7f3a2117f9f37
adelebrgx <adele.bourgeix@gmail.com>
2020-11-02 13:50:33 +0100

    set express app

--- api/app.js
+++ api/app.js
@@ -4,7 +4,16 @@ const mongoose= require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//create express app
+const app = express();
+
+
+//connect to database
 mongoose.connect(process.env.MONGODB,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
     console.log('connected to database');
+});
+
+app.listen(process.env.PORT_API, () => {
+    console.log('now listening for requests on port 4000');
 });
