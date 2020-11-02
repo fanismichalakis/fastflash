fd0ea1db1a12e977f00811fd1bd4c20676acb7b0
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-02 16:50:49 +0100

    comment all appolo import

--- fastflash/src/main.js
+++ fastflash/src/main.js
@@ -1,27 +1,32 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 
-import ApolloClient from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import VueApollo from "vue-apollo";
+createApp(App).mount('#app');
+
+// import Vue from "vue";
+// import App from "./App.vue";
+
+// import ApolloClient from "apollo-client";
+// import { InMemoryCache } from "apollo-cache-inmemory";
+// import VueApollo from "vue-apollo";
 
 
-Vue.use(VueApollo);
+// Vue.use(VueApollo);
 
 
-const client = new ApolloClient({
-    link: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache({
-        addTypename: true
-    })
-});
+// const client = new ApolloClient({
+//     link: 'http://localhost:4000/graphql',
+//     cache: new InMemoryCache({
+//         addTypename: true
+//     })
+// });
 
-const apolloProvider = new VueApollo({
-    defaultClient: client,
-})
+// const apolloProvider = new VueApollo({
+//     defaultClient: client,
+// })
 
-new Vue({
-    apolloProvider,
-    render: h => h(App)
-}).$mount("#app");
+// new Vue({
+//     apolloProvider,
+//     render: h => h(App)
+// }).$mount("#app");
 
