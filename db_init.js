d64729ac5fe4c479b5994f93fc88e324e0c81602
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-04 15:40:16 +0100

    async process for init db

--- db_init.js
+++ db_init.js
@@ -4,14 +4,12 @@ const db_data = require('./db_data.json');
 const { exists } = require('./api/models/card');
 
 mongoose.connect(process.env.MONGODB,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
-mongoose.connection.once('open', () => {
+mongoose.connection.once('open', async () => {
   console.log('connected to database');
   console.log('Initializing the db by erasing everything then populate it');
-  Card.remove({});
-  Card
-    .insertMany(db_data)
-    .then(() => {
-      console.log('data inserted');
-      process.exit();
-    });
+  await Card.remove({});
+  await Card.insertMany(db_data);
+  console.log('data inserted');
+  process.exit();
+
 });
