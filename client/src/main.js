64e495d2b23be4fe9ff0e59b95a294de35c8af7f
adelebrgx <adele.bourgeix@gmail.com>
2020-11-04 12:07:06 +0100

    set up apollo

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
+
+Vue.use(VueApollo)
+
+
+
+const apolloClient = new ApolloClient({
+  uri: "'http://localhost:4000/graphql'"
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
