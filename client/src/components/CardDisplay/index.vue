95ce0818662d601fd5ec31f74c2c1f8e2381b2f1
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-04 16:12:36 +0100

    cards_id fix graphql query

--- client/src/components/CardDisplay/index.vue
+++ client/src/components/CardDisplay/index.vue
@@ -1,14 +1,14 @@
 <template>
   <SingleCard class="card"
     v-bind:cards_id="cards_id"
-    v-bind:cards_info="cards_info"
     v-bind:current_index="current_index"
     v-on:next-card="nextCard"
   />
 </template>
 
 <script>
-import SingleCard from './SingleCard'
+import SingleCard from './SingleCard';
+import gql from 'graphql-tag';
 
 export default {
   name: 'DisplayCard',
@@ -26,49 +26,20 @@ export default {
       }
     }
   },
+  apollo: {
+    cards_id: {
+      query: gql`{
+        cards {
+          id
+        }
+      }`,
+      update: data => data.cards
+    },
+  },
   data() {
     return {
       current_index: 0,
-      cards_id: ['1', '1001', '1002', '1003'],
-      cards_info: {
-        '1000': {
-          recto:'manger',
-          verso:'eat',
-          recto_def:'français',
-          verso_def:'anglais',
-          position:1,
-          library:'franglais',
-          id:1000
-        },
-        '1001': {
-          recto:'bouger',
-          verso:'move',
-          recto_def:'français',
-          verso_def:'anglais',
-          position:1,
-          library:'franglais',
-          id:1001
-        },
-        '1002': {
-          recto:'chien',
-          verso:'dog',
-          recto_def:'français',
-          verso_def:'anglais',
-          position:1,
-          library:'franglais',
-          id:1002
-        },
-        '1003': {
-          recto:'jaune',
-          verso:'yellow',
-          recto_def:'français',
-          verso_def:'anglais',
-          position:1,
-          library:'franglais',
-          id:1003
-        }
-      },
-      cards: [],
+      cards_id: []
     };
   },
 }
