f720f356a183f8557cf977936b671493342f47a9
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-04 08:29:56 +0100

    add morgan and ping route to test if back is up

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose= require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
+
 dotenv.config();
 
 const schema = require('./schemas/schema');
@@ -9,12 +11,17 @@ const schema = require('./schemas/schema');
 //create express app
 const app = express();
 
+app.use(morgan('combined'));
 
 //connect to database
 mongoose.connect(process.env.MONGODB,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 });
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+})
 
 
 app.use('/graphql', graphqlHTTP({
