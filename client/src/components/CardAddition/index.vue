1edb2b0825c7044e0c628a1ed88ea0a58e61a2ef
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-05 08:28:48 +0100

    clean the component, remove useless parts

--- client/src/components/CardAddition/index.vue
+++ client/src/components/CardAddition/index.vue
@@ -1,27 +1,14 @@
 <template>
-    <AddCard class="add-card"
-      v-on:add-card="addCard"
-    />
+    <AddCard class="add-card" />
 </template>
 
 <script>
 import AddCard from './AddCard'
-import gql from 'graphql-tag';
 
 export default {
   name: 'CardAddition',
   components: {
     AddCard,
-  },
-  apollo: {
-    cards_id: {
-      query: gql`{
-        cards {
-          id
-        }
-      }`,
-      update: data => data.cards
-    },
   }
 }
 </script>
